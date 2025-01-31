fc0b0538dc1b7612875885d053773bda53f1041d
gnanashekar2004 <gnanashekar2004@kgpian.iitkgp.ac.in>
2025-02-01 00:37:15 +0530

    Multi language translation done

--- faq_app/models.py
+++ faq_app/models.py
@@ -1,23 +1,37 @@
+# import asyncio
 from django.db import models
 from ckeditor.fields import RichTextField
+from googletrans import Translator
 
 class FAQ(models.Model):
     question = models.TextField()
-    answer = RichTextField() # Rich text support for WYSIWYG editor
-    question_hi = models.TextField(blank=True, null=True) # Hindi transalation
+    answer = RichTextField()
+    question_hi = models.TextField(blank=True, null=True)
     answer_hi = RichTextField(blank=True, null=True)
-    question_bn = models.TextField(blank=True, null=True) # Bengali transalation
+    question_bn = models.TextField(blank=True, null=True)
     answer_bn = RichTextField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        translator = Translator()
+        if not self.question_hi:
+            self.question_hi = translator.translate(self.question, src='en', dest='hi').text
+        if not self.question_bn:
+            self.question_bn = translator.translate(self.question, src='en', dest='bn').text
+        if not self.answer_hi:
+            self.answer_hi = translator.translate(self.answer, src='en', dest='hi').text
+        if not self.answer_bn:
+            self.answer_bn = translator.translate(self.answer, src='en', dest='bn').text
+        super().save(*args, **kwargs)
 
     def get_translation(self, lang='en'):
         """
-        Returns the translation question and answer based on the requested language.
-        Defaults to english if translation is unavailable for the requested language.
+        Returns the translated question and answer based on the requested language.
+        Defaults to English if translation is unavailable.
         """
         return {
-            'question': getattr(self, f'question_{lang}',self.question),
-            'answer': getattr(self,f'answer_{lang}',self.answer),
+            'question': getattr(self, f'question_{lang}', self.question),
+            'answer': getattr(self, f'answer_{lang}', self.answer),
         }
 
     def __str__(self):
-        return self.question #Displays question as a string
+        return self.question
