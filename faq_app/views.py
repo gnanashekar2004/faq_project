27cd704edb1266c712976db67d18016c5c4b70d1
gnanashekar2004 <gnanashekar2004@kgpian.iitkgp.ac.in>
2025-02-01 01:03:29 +0530

    Caching with redis done

--- faq_app/views.py
+++ faq_app/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import FAQ
@@ -10,7 +11,13 @@ class FAQListView(APIView):
     """
 
     def get(self, request):
-        lang = request.GET.get('lang', 'en') # Get requested language
-        faqs = FAQ.objects.all() # Fetch all FAQs
-        data = [faq.get_translation(lang) for faq in faqs] # Get translations for each FAQ
-        return Response(data) # Return JSON response
+        lang = request.GET.get('lang', 'en')
+        cache_key = f'faqs_{lang}'  # Unique cache key for each language
+        data = cache.get(cache_key)  # Try retrieving from cache
+
+        if not data:  # If cache is empty, fetch from DB
+            faqs = FAQ.objects.all()
+            data = [faq.get_translation(lang) for faq in faqs]
+            cache.set(cache_key, data, timeout=3600)  # Cache for 1 hour (3600 sec)
+
+        return Response(data)
